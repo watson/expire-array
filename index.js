8ebf6654a6c343775f23f298e8938767f75a1097
Thomas Watson Steen <w@tson.dk>
2015-03-26 17:35:21 +0000

    Add DEBUG=expire-array option

--- index.js
+++ index.js
@@ -1,31 +1,38 @@
 'use strict'
 
+var debug = require('debug')('expire-array')
 var delay = 5 // ms delay to wait before scheduling a GC after an element should be cleared
 
 var ExpireArray = module.exports = function (timeout) {
   if (!(this instanceof ExpireArray)) return new ExpireArray(timeout)
   if (!Number.isFinite(timeout)) throw new Error('Timeout must be a number')
+  debug('init with %d ms expiry', timeout)
   this._timeout = timeout
   this._db = []
 }
 
 ExpireArray.prototype._scheduleGC = function () {
   if (this._nextGC) return
+  if (this._db.length) debug('next element expires in %d ms', this._db[0][0] - Date.now())
   var ts = this._db.length ? this._db[0][0] - Date.now() + delay : this._timeout
+  debug('schedule GC in %d ms', ts)
   this._nextGC = setTimeout(this._gc.bind(this), ts)
   if (this._nextGC.unref) this._nextGC.unref()
 }
 
 ExpireArray.prototype._gc = function () {
+  debug('running GC on %d elements', this._db.length)
   var now = Date.now()
   while (this._db.length && this._db[0][0] < now) {
     this._db.shift()
   }
+  debug('%d elements remain after GC', this._db.length)
   this._nextGC = null
   this._scheduleGC()
 }
 
 ExpireArray.prototype.push = function (elm) {
+  debug('push')
   if (!this._db.length) this._scheduleGC()
   this._db.push([Date.now() + this._timeout, elm])
 }
